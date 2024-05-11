b13a6aa9bc16047e70fa3b582ffdf7426b6c0347
Kim, yongwook <kyw4846@gmail.com>
2024-05-11 20:44:33 +0900

    피드백 수정

--- kimyongwook/my-app/src/index.js
+++ kimyongwook/my-app/src/index.js
@@ -3,34 +3,34 @@ import ReactDOM from 'react-dom';
 import './App.css';
 
 function Counter() {
-  const [input, setInput] = useState([]);
-  const [name, setName] = useState('');
+  const [restaurants, setRestaurants] = useState([]); // 변수명 변경
+  const [restaurantName, setRestaurantName] = useState(''); 
 
   const handleInputChange = (e) => {
-    setName(e.target.value); // 입력값 name 변수에 설정
+    setRestaurantName(e.target.value); // 입력값 name 변수에 설정
   };
 
-  const addInput = () => {
-    if (name !== '') {
-      setInput([name, ...input]); // 이전 배열 맨 앞에 새 name을 추가
-      setName('');
+  const addRestaurant = () => {
+    if (restaurantName.trim() !== '') { // *** 은혜님이 알려주신 trim() 으로 공백 등록 막기 ***
+      setRestaurants([restaurantName, ...restaurants]); // 이전 배열 맨 앞에 새 name을 추가
+      setRestaurantName('');
     }
   };
 
-  const deleteInput = (idx) => {
-    setInput(input.filter((_, index) => index !== idx)); // idx는 false가 되어 새 배열에서 제외
+  const deleteRestaurant = (idx) => {
+    setRestaurants(restaurants.filter((_, index) => index !== idx)); // idx는 false가 되어 새 배열에서 제외
   };
 
   return (
     <div className="counter-container">
       <p className="counter-text">내 맛집 리스트</p>
-      <input type="text" className="input" placeholder="맛집 이름 입력" value={name} onChange={handleInputChange} />
-      <button className="counter-button" onClick={addInput}>+</button>
+      <input type="text" className="input" placeholder="맛집 이름 입력" value={restaurantName} onChange={handleInputChange} />
+      <button className="counter-button" onClick={addRestaurant}>+</button>
       <ul className="input-list">
-      {input.map((input, idx) => ( // input 배열 매핑
-          <div key={idx}>
-            <p>{input}<button className="delete-button" onClick={() => deleteInput(idx)}>삭제</button></p>
-          </div>
+      {restaurants.map((restaurant, idx) => ( // input 배열 매핑
+          <li key={idx}> 
+            <p>{restaurant}<button className="delete-button" onClick={() => deleteRestaurant(idx)}>삭제</button></p>
+          </li> // div -> li
         ))}
       </ul>
     </div>
